6bd74c50d7fe00f1bcd62c740b18d9a9c41f64e5
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2023-04-30 16:06:07 +0700

    feat: add regex classification to user query

---
+++ src/backend/src/main/regex.js
@@ -0,0 +1,26 @@
+/* ------------------------------ File : regex.js ------------------------------ */
+/*   Do the pattern-matching to the query from user using regular expression     */
+
+function handleQuery(query) {
+    if (/[\s\(\)]*(\-|\s*)(\d+|\d+\.\d+)([\s\(\)]*[\+\-\*/\^][\s\(\)]*(\-|\s*)(\d+|\d+\.\d+)[\s\(\)]*)*\?*\s*/g.test(query)) {
+        // Calculate the mathematical expression
+        console.log("mungkin 7")
+    }
+    else if (/\s*\d{1,2}\s*\/\s*\d{1,2}\s*\/\s*\d{4}\s*(\?)*\s*/.test(query) ||
+             /\s*hari\s+apa\s+\d{1,2}\s*\/\s*\d{1,2}\s*\/\s*\d{4}\s*(\?)*\s*/i.test(query)) {
+        // Search for the name of the day from the asked date
+        console.log("mungkin kamis")
+    }
+    else if (/\s*tambahkan\s+pertanyaan\s+\S+\s+dengan\s+jawaban\s+\S+\s*/i.test(query)) {
+        // Add the question and its corresponding answer to database
+        console.log("udah ditambah")
+    }
+    else if (/\s*hapus\s+pertanyaan\s+\S+\s*/i.test(query)) {
+        // Delete the question and its corresponding answer from database
+        console.log("udah kehapus")
+    }
+    else {
+        // Check if the question is in the database and give an appropriate answer to it
+        console.log("bentar nyocokin dulu")
+    }
+}
