3cd8feeae332a128807e99d129f9e3e878fadf8d
Margaretha Olivia <margarethaolivia41@gmail.com>
2023-04-30 22:03:44 +0700

    feat: connect database to frontend (send button)

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,60 +1,39 @@
 import { useState, useEffect, React } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import axios from "axios";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const [tabs, setTabs] = useState([]);
+  const [send, setSend] = useState(false);
+  const [activeTab, setActiveTab] = useState("");
+  let maxChatId = 0;
 
   // Fetching message from backend on mount
   useEffect(() => {
-    fetch("http://localhost:4000")
+    fetch("http://localhost:4000/chat")
       .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
+      .then((data) => {
+        const minChatId = Math.min(...data.map((item) => item.chat_id));
+        const maxChatId = Math.max(...data.map((item) => item.chat_id));
 
-  const [activeTab, setActiveTab] = useState("tab1");
+        const tabs = [];
+        for (let i = minChatId; i <= maxChatId; i++) {
+          tabs.push({ id: i.toString(), title: `Tab ${i}` });
+        }
 
-  const tabs = [
-    { id: "1", title: "Tab 1" },
-    { id: "2", title: "Tab 2" },
-    { id: "3", title: "Tab 3" },
-    { id: "4", title: "Tab 4" },
-    { id: "5", title: "Tab 5" },
-    { id: "6", title: "Tab 6" },
-    { id: "7", title: "Tab 7" },
-    { id: "8", title: "Tab 8" },
-    { id: "9", title: "Tab 9" },
-    { id: "10", title: "Tab 10" },
-    { id: "11", title: "Tab 11" },
-    { id: "12", title: "Tab 12" },
-    { id: "13", title: "Tab 13" },
-  ];
+        setChats(data);
+        setTabs(tabs);
+      });
+  }, [send]);
 
-  const messages = [
-    { id: 1, question: "siapa pengampu matkul stima K1?", answer: "pak rin" },
-    { id: 1, question: "siapa pengampu matkul stima K2?", answer: "bu ulfa" },
-    { id: 2, question: "kenapa tubes banyak?", answer: "biar keos" },
-    { id: 2, question: "dimana spongebob tinggal?", answer: "bikini bottom" },
-    {
-      id: 2,
-      question:
-        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available (mau coba pertanyaan yang panjang, jadinya bisa berbaris-baris ga ya?)",
-      answer:
-        "yoi bisa dong, ini juga mau coba jawaban yang panjang, tampilannya kurang lebih kayak gini",
-    },
-    {
-      id: 3,
-      question: "apa kepanjangan dari ITB",
-      answer: "Institut Teknologi Bandung",
-    },
-    { id: 2, question: "bagaimana cara membuat chatGPT?", answer: "pake AI" },
-    { id: 3, question: "siapa saudaranya upin?", answer: "ipin" },
-    {
-      id: 1,
-      question: "apa yang mempunyai 12 kaki dan bisa terbang?",
-      answer: "6 ekor burung",
-    },
-    { id: 2, question: "apa yang ada di ujung langit?", answer: "huruf 't'" },
-  ];
+  const handleNewTab = async (e) => {
+    e.preventDefault();
+    setActiveTab(...tabs, {
+      id: maxChatId.toString(),
+      title: `Tab ${maxChatId}`,
+    });
+  };
 
   return (
     <Router>
@@ -63,8 +42,15 @@ function App() {
           <Link to="/">
             <h1 className="font-bold text-lg mb-4">ChatDOA</h1>
           </Link>
-          <nav className="h-3/4 overflow-auto">
-            <ul className="flex flex-col h-full space-y-4 pr-4 pb-8">
+          <form className="flex items-center mt-auto" onSubmit={handleNewTab}>
+            <div className="flex flex-col items-start pb-4">
+              <button className="bg-blue-500 hover:bg-blue-600 text-white rounded-lg px-4 py-2">
+                New Tab
+              </button>
+            </div>
+          </form>
+          <nav className="h-4/6 overflow-auto">
+            <ul className="flex flex-col h-full space-y-4 pr-4 pb-8 ">
               {tabs.map((tab) => (
                 <li
                   key={tab.id}
@@ -86,7 +72,12 @@ function App() {
           </nav>
           <div className="flex flex-col items-start mx-3 mt-5">
             <label className="inline-flex items-center m-2">
-              <input type="radio" className="form-radio" name="radio-option" />
+              <input
+                type="radio"
+                className="form-radio"
+                name="radio-option"
+                checked
+              />
               <span className="ml-2">KMP</span>
             </label>
             <label className="inline-flex items-center m-2">
@@ -104,9 +95,13 @@ function App() {
                 path={`/${tab.id}`}
                 element={
                   <ChatWindow
-                    messages={messages.filter(
-                      (msg) => msg.id === parseInt(tab.id)
+                    key={tab.id}
+                    id={tab.id}
+                    messages={chats.filter(
+                      (msg) => msg.chat_id === parseInt(tab.id)
                     )}
+                    send={send}
+                    setSend={setSend}
                   />
                 }
               />
@@ -118,7 +113,7 @@ function App() {
   );
 }
 
-function ChatWindow({ messages }) {
+function ChatWindow({ id, messages, send, setSend }) {
   const [question, setQuestion] = useState("");
 
   useEffect(() => {
@@ -129,9 +124,21 @@ function ChatWindow({ messages }) {
     setQuestion(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // handle form submission
+    try {
+      const response = await axios.post(`http://localhost:4000/chat/${id}`, {
+        question: question,
+        answer: "jawaban default",
+      });
+      console.log(response.data);
+      console.log(id);
+      setSend(!send);
+      // Update UI or display success message to the user
+    } catch (error) {
+      console.error(error.message);
+      // Update UI or display error message to the user
+    }
   };
 
   return (
